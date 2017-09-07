39ce7b6e65bedae9030f981c523dd92a2ccc841a
PivithuruThejan <pivithuru.14@cse.mrt.ac.lk>
2017-09-07 18:56:30 +0530

    Changed package name

--- filter-benchmark/target/generated-sources/annotations/com/jenkov/generated/FilterBenchmark_StartUpState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.jenkov.generated;
-public class FilterBenchmark_StartUpState_jmhType extends FilterBenchmark_StartUpState_jmhType_B3 {
-}
-
--- filter-benchmark/target/generated-sources/annotations/com/jenkov/generated/FilterBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.jenkov.generated;
-public class FilterBenchmark_jmhType extends FilterBenchmark_jmhType_B3 {
-}
-
